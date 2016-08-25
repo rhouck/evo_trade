240934a29a1f1adce4ef77a9724832ce63fee865
Ryan Houck <ryan@levelskies.com>
2016-08-24 20:44:05 -0700

    switched select function in favor of one that penalizes tree complexity

--- tb.py
+++ tb.py
@@ -7,7 +7,7 @@ from deap import gp
 from deap import tools
 
 
-def load_toolbox(weights, pset, pxs, eval_func, tournsize, height_range=(2,4)):
+def load_toolbox(weights, pset, pxs, eval_func, tournsize, parsimony_size, height_range):
     creator.create("Fitness", base.Fitness, weights=weights)
     creator.create("Individual", gp.PrimitiveTree, fitness=creator.Fitness)
 
@@ -17,15 +17,11 @@ def load_toolbox(weights, pset, pxs, eval_func, tournsize, height_range=(2,4)):
     toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("compile", gp.compile, pset=pset)
-
     toolbox.register("evaluate", eval_func, pxs=pxs)
-    #infeas_vals = (np.nan for i in range(len(weights)))
-    #toolbox.decorate("evaluate", tools.DeltaPenality(feasibility_func, infeas_vals))
-    toolbox.register("select", tools.selTournament, tournsize=tournsize)
+    #toolbox.register("select", tools.selTournament, tournsize=tournsize)
+    toolbox.register("select", tools.selDoubleTournament, fitness_size=tournsize, 
+                     parsimony_size=parsimony_size, fitness_first=True)
     toolbox.register("mate", gp.cxOnePoint)
     toolbox.register("expr_mut", gp.genFull, min_=0, max_=height_range[1])
     toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
-
-    #toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=height_range[1]))
-    #toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=height_range[1]))
     return toolbox
