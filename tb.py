85ccd88a2a4517cdf4efe2cca5655be2be43581b
Ryan Houck <ryan@levelskies.com>
2016-08-29 19:45:45 -0700

    added back limit to tree height after mutation and mating

--- tb.py
+++ tb.py
@@ -24,4 +24,8 @@ def load_toolbox(weights, pset, dr, eval_func, tournsize, parsimony_size, height
     toolbox.register("mate", gp.cxOnePoint)
     toolbox.register("expr_mut", gp.genFull, min_=0, max_=height_range[1])
     toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+    
+    toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=height_range[1]))
+    toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=height_range[1]))
+
     return toolbox
