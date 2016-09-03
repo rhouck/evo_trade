251e5718287fea540830e5519e58be90961b4d59
Ryan Houck <ryan@levelskies.com>
2016-09-02 19:10:40 -0700

    limited mutation size to 0-1

--- tb.py
+++ tb.py
@@ -22,7 +22,7 @@ def load_toolbox(weights, pset, dr, eval_func, tournsize, parsimony_size, height
     toolbox.register("select", tools.selDoubleTournament, fitness_size=tournsize, 
                      parsimony_size=parsimony_size, fitness_first=True)
     toolbox.register("mate", gp.cxOnePoint)
-    toolbox.register("expr_mut", gp.genFull, min_=0, max_=height_range[1])
+    toolbox.register("expr_mut", gp.genFull, min_=0, max_=1)#height_range[1])
     toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
     
     toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=height_range[1]))
