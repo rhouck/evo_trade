1e5b34aab9efa4ba9050a9ccf22183a6d1e8284b
Ryan Houck <ryan@levelskies.com>
2016-09-06 21:36:23 -0700

    removed default vals in torunament

--- tournament.py
+++ tournament.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 from deap import algorithms
 
-from utils import drop_id_funcs, aggregate_scores
+from utils import drop_id_funcs, aggregate_attr
 
 
 def get_seed_state():
@@ -49,7 +49,7 @@ count_unique = lambda x: float(len(set(x)))
 
 
 def run_tournament(pop, toolbox, cxpb, mutpb, ngen, stats, hof, log, 
-                   checkpoint_fn, start_gen, pset, randstate=get_seed_state(), cp_freq=5, seed_scale=3):
+                   checkpoint_fn, start_gen, pset, cp_freq, seed_scale, randstate=get_seed_state()):
     
     random.setstate(randstate)
     start_len = len(pop) / seed_scale
@@ -57,6 +57,7 @@ def run_tournament(pop, toolbox, cxpb, mutpb, ngen, stats, hof, log,
         
         
         pop_start = [ind for ind in pop]
+
         #n = count_unique([name_to_int(i) for i in pop_start])
         #print('pop_start:\t{0}\t{1}'.format(len(pop_start), n))
         pop = algorithms.varAnd(pop, toolbox, cxpb=cxpb, mutpb=mutpb)
@@ -89,6 +90,52 @@ def run_tournament(pop, toolbox, cxpb, mutpb, ngen, stats, hof, log,
 
         #n = count_unique([name_to_int(i) for i in pop])
         #print('sel pop:\t{0}\t{1}'.format(len(pop), n))
+
+        # log stats
+        record = stats.compile(pop)
+        hof.update(pop)
+        log.record(gen=gen, evals=len(invalid_ind), **record)
+        print(log.stream)
+
+        if (gen + 1) % cp_freq == 0:
+            checkpoint_tournament(checkpoint_fn, pop, gen, hof, log, 
+                                  random.getstate())
+  
+    return pop, hof, log
+
+
+def run_add_tournament(pop, toolbox, cxpb, mutpb, ngen, stats, hof, log, 
+                   checkpoint_fn, start_gen, pset, cp_freq, seed_scale, randstate=get_seed_state()):
+    
+    random.setstate(randstate)
+    start_len = len(pop)
+    for gen in range(start_gen, start_gen + ngen):
+        
+        
+        pop_start = [ind for ind in pop]
+        agg_pop_start = {i: aggregate_attr(pop, i) for i in ( 'scores','holdings')}
+        agg_pop_start['len'] = len(pop)
+
+        pop = algorithms.varAnd(pop, toolbox, cxpb=cxpb, mutpb=mutpb)
+        pop.extend(pop_start)
+        pop = [drop_id_funcs(ind, toolbox, pset) for ind  in pop]
+
+        # evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit['objectives']
+            for i in ('scores', 'holdings'):
+                setattr(ind, i, fit[i])
+
+        # filt invalid and upsample to replace invalid inds
+        pop = [ind for ind in pop if not is_nan(ind)]
+        #pop = upsample_pop(pop, start_len)
+        
+        # drop duplicates
+        pop = list(pd.DataFrame([(i, i.__str__()) for i in pop]).drop_duplicates(1)[0])
+        
+        pop = toolbox.select(pop, k=start_len)
 
         # log stats
         record = stats.compile(pop)
